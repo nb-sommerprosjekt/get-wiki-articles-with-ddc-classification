383ef74422eabbe0ad273a2b4e3eb40b49a04a8b
nb-sommerprosjekt <auwalsoe@gmail.com>
2017-09-22 13:41:13 +0200

    Removed some unused code

--- getArticles.py
+++ getArticles.py
@@ -2,11 +2,11 @@ import wikipedia
 from bs4 import BeautifulSoup
 import requests
 def get_articles():
+
     wikipedia.set_lang("no")
     sok = wikipedia.search('"Eksterne baser (Autoritetsdata) GND"', results = 5000)
     pages = []
     error_searchTerm = open("error_file.txt","w")
-    url_file = open("url_wiki_save.txt","w")
     for title in sok:
 
         try:
@@ -28,8 +28,7 @@ def get_articles():
         meta_file.close()
 
 def get_gnd_url_from_html(html_doc):
-    #html_doc = open(html_doc_path, "r")
-    #print("lololol")
+
     soup = BeautifulSoup(html_doc,'html.parser')
 
     for link  in soup.find_all('a'):
@@ -51,7 +50,7 @@ def get_dewey_from_gnd (url):
         for element in td:
             if "DDC-Notation" in element:
                 td_all.append(next(td_soup))
-    dewey =[]
+
     for element in td_all:
         dewey = str(element).replace('<td>','')
         dewey = dewey.replace('</td>','')
@@ -64,14 +63,6 @@ def get_dewey_from_gnd (url):
         return dewey_u_html
 
 
-    #print(len(td_all))
-    #print(type(td_all))
-    #print(len(td_all[9]))
-    #print(type(dewey))
-    #print(dewey)
-    #print(type(td_all[0]))
-    #print(len(td_all))
 
 if __name__ == '__main__':
     get_articles()
-    #get_gnd_url_from_htlm()
